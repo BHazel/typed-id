67d9f90082a8e8f99128369eb81d5e6438bcb3e3
Benedict Hazel <bwhazel@gmail.com>
2023-11-11 13:56:23 +0000

    Add property builder extension to support the typed string ID in EF Core.

---
+++ src/src/BWHazel.Data.TypedId.EntityFrameworkCore/PropertyBuilderExtensionsForStringId.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using BWHazel.Data;
+
+namespace Microsoft.EntityFrameworkCore;
+
+/// <summary>
+/// Defines extension methods for <see cref="PropertyBuilder{TProperty}"/> to support the <see cref="StringId{T}"/> typed ID.
+/// </summary>
+public static class PropertyBuilderExtensionsForStringId
+{
+    /// <summary>
+    /// Configures the property so the typed ID property value is converted to and from the database as a string.
+    /// </summary>
+    /// <typeparam name="T">The entity type.</typeparam>
+    /// <param name="propertyBuilder">The <see cref="PropertyBuilder{TProperty}"/> instance.</param>
+    /// <returns>The same builder instance so that multiple configuration calls can be chained.</returns>
+    public static PropertyBuilder<StringId<T>> IsTypedStringId<T>(this PropertyBuilder<StringId<T>> propertyBuilder)
+    {
+        return propertyBuilder.HasConversion(
+            idToDb => idToDb.Value,
+            idFromDb => new(idFromDb)
+        );
+    }
+}
